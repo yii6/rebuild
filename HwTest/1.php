cf1a8723e1242c0e31579c27400f050197179a0d
yu.wang <i@yii6.com>
2025-11-11 16:46:54 +0800

    1

--- HwTest/1.php
+++ HwTest/1.php
@@ -1,6 +1,7 @@
 <?php
 
 declare(strict_types=1);
+
 $in = @fopen('1.txt', 'rb');
 if ($in === false) {
     $in = STDIN;
@@ -10,39 +11,59 @@ $line = trim(fgets($in));
 $n = (int)$n;
 $T = (int)$T;
 $k = (int)$k;
-$ans = 0;
 $calories = explode(',', substr($str, 1, -1));
+$calories = array_map('intval', $calories);
 
-// 是否是k个运动
-function isK($str, $k)
+function subsetSumsByCountDP(array $arr): array
 {
-    $n = strlen($str);
-    $tmp = 0;
-    $excises = [];
-    for ($i = 0; $i < $n; ++$i) {
-        if ($str[$i] === '1') {
-            ++$tmp;
-            $excises[] = $i;
+    $m = count($arr);
+    // 初始化：选 0 个时，和为 0 的方案有 1 种
+    $buckets = array_fill(0, $m + 1, []);
+    $buckets[0][0] = 1;
+    $maxCnt = 0; // 当前已处理元素能达到的最大 cnt
+    foreach ($arr as $v) {
+        // 倒序遍历 cnt，避免本轮更新污染下一轮读取
+        for ($cnt = $maxCnt; $cnt >= 0; --$cnt) {
+            foreach ($buckets[$cnt] as $sum => $freq) {
+                $ns = $sum + $v;
+                $buckets[$cnt + 1][$ns] = ($buckets[$cnt + 1][$ns] ?? 0) + $freq;
+            }
         }
+        ++$maxCnt;
     }
-    return $tmp === $k ? $excises : false;
+    return $buckets;
 }
 
-function totalCalories($excises, $calories): int
-{
-    $calorie = 0;
-    foreach ($excises as $j) {
-        $calorie += (int)$calories[$j];
-    }
-    return $calorie;
-}
+// 折半
+$mid = intdiv($n, 2);
+$left = array_slice($calories, 0, $mid);
+$right = array_slice($calories, $mid);
 
-for ($i = 0; $i < 1 << $n; ++$i) {
-    $str = str_pad(decbin($i), $n, '0', STR_PAD_LEFT);
-    if ($excises = isK($str, $k)) {
-        $calorie = totalCalories($excises, $calories);
-        if ($calorie === $T) {
-            ++$ans;
+// 两边都预处理为：选中个数 => (sum => 方案数)
+$leftBuckets = subsetSumsByCountDP($left);
+$rightBuckets = subsetSumsByCountDP($right);
+
+// 统计答案
+$ans = 0;
+$maxL = count($left);
+$maxR = count($right);
+
+// 枚举左边选中个数 cntL，右边需要选 needCnt = k - cntL
+for ($cntL = 0; $cntL <= min($k, $maxL); ++$cntL) {
+    $needCnt = $k - $cntL;
+    if ($needCnt > $maxR) {
+        continue;
+    }
+    if (empty($leftBuckets[$cntL]) || empty($rightBuckets[$needCnt])) {
+        continue;
+    }
+    $leftMap = $leftBuckets[$cntL];
+    $rightMap = $rightBuckets[$needCnt];
+    // 遍历左边的每个 sumL，查右边 (T - sumL)
+    foreach ($leftMap as $sumL => $freqL) {
+        $needSum = $T - $sumL;
+        if (isset($rightMap[$needSum])) {
+            $ans += $freqL * $rightMap[$needSum];
         }
     }
 }
